0f427c1acfeb939cabb20da158462c291b0c1a23
Gamliel Urlanda <g.urlanda@gmail.com>
2023-06-16 20:44:39 -0700

    Improve typography for section 1

--- codebase/client/src/components/pages/Home/Section1.tsx
+++ codebase/client/src/components/pages/Home/Section1.tsx
@@ -12,7 +12,7 @@ const SectionItem: React.FC<SecItmProps> = ({ heading, details, icon }) => {
       <div
         className={`h-36 w-full bg-no-repeat bg-center text-center mb-2 py-12 ${icon}`}
       ></div>
-      <h2 className="font-bold text-lg">{heading}</h2>
+      <h2 className="font-bold text-lg text-center">{heading}</h2>
       <h3 className="text-md text-center">{details}</h3>
     </div>
   );
@@ -28,7 +28,7 @@ const Section1: React.FC = () => {
       {/* Items */}
       <div className="flex flex-col justify-around px-4 lg:flex-row">
         <SectionItem
-          heading="Healthy meals designed by a registered dietitian"
+          heading="Healthy meals designed by a licensed dietitian"
           details="Meals are formulated to be heart-healthy and diabetes-friendly."
           icon="bg-laptop-icon"
         />
