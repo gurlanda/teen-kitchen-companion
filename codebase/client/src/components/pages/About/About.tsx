10d1a245fd6061b8fe47aa846115157785267f7f
Gamliel Urlanda <g.urlanda@gmail.com>
2023-06-16 23:55:33 -0700

    About: Simplify styles

--- codebase/client/src/components/pages/About/About.tsx
+++ codebase/client/src/components/pages/About/About.tsx
@@ -4,15 +4,7 @@ import TkpBanner from 'src/components/layout/TkpBanner';
 const AboutParagraph: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  return <p className="mb-4 leading-relaxed text-gray-800">{children}</p>;
-};
-
-const ImgGridItem: React.FC<{ src: string }> = ({ src }) => {
-  return (
-    <div className="w-32 flex flex-col justify-center">
-      <img className="w-full h-auto" src={src} alt="" />
-    </div>
-  );
+  return <p className="leading-relaxed text-gray-800">{children}</p>;
 };
 
 const About: React.FC = () => {
@@ -22,8 +14,9 @@ const About: React.FC = () => {
       <TkpBanner normalWeight>
         Building Healthier Communities Through Food
       </TkpBanner>
-      <div className="tk-acumin-pro-semi-condensed flex flex-col items-center px-4 mx-auto mb-32 xs:px-6 sm:px-9 md:max-w-3xl">
-        <h1 className="tk-acumin-pro-condensed text-gray-900 text-center text-4xl mb-6 mt-10">
+
+      <div className="flex flex-col gap-4 max-w-[min(90vw,65ch)] mx-auto tk-acumin-pro-semi-condensed">
+        <h1 className="tk-acumin-pro-condensed text-gray-900 text-center text-4xl">
           About the Teen Kitchen Project
         </h1>
         <AboutParagraph>
@@ -45,22 +38,6 @@ const About: React.FC = () => {
           Soquel, California. We are supported through donations from the
           community, grants, and contracts with local and state agencies.
         </AboutParagraph>
-        {/* <h3 className="my-10 text-2xl text-center tk-acumin-pro-condensed">
-          Please support the businesses that support the Teen Kitchen Project:
-        </h3>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-5">
-          {businessSupporters.map((src) => (
-            <ImgGridItem src={src} key={createId()} />
-          ))}
-        </div>
-        <h3 className="mt-14 mb-10 text-3xl text-center tk-acumin-pro-condensed">
-          Thank you to the following major funders of our work:
-        </h3>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-5">
-          {majorFunders.map((src) => (
-            <ImgGridItem src={src} key={createId()} />
-          ))}
-        </div> */}
       </div>
     </>
   );
