05bfbdead2b06c3fe47fca08c5b4892b55eb06cb
Gamliel Urlanda <g.urlanda@gmail.com>
2023-06-16 20:57:11 -0700

    Plug in new photo and improve typography in section 2

--- codebase/client/src/components/pages/Home/Section2.tsx
+++ codebase/client/src/components/pages/Home/Section2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import deliveryAngels from '../../../assets/img/deliveryAngels.jpg';
+import mealPrep from 'src/assets/img/single-person-meal-prep.png';
 
 interface SecItmProps {
   heading: string;
@@ -8,7 +8,7 @@ interface SecItmProps {
 
 const SectionItem: React.FC<SecItmProps> = ({ heading, details }) => {
   return (
-    <div className="flex flex-col items-center my-2 md:basis-0 md:grow max-w-lg">
+    <div className="flex flex-col justify-center items-center my-2 md:basis-0 md:grow max-w-lg">
       <h2 className="font-bold text-lg">{heading}</h2>
       <h3 className="text-md text-center">{details}</h3>
     </div>
@@ -21,7 +21,7 @@ interface IGProps {
 
 const ItemGroup: React.FC<IGProps> = ({ children }) => {
   return (
-    <div className="flex flex-col items-center justify-between px-6 md:flex-row lg:flex-col">
+    <div className="flex flex-col items-center justify-between px-6 md:flex-row lg:flex-col basis-0 grow-[3]">
       {children}
     </div>
   );
@@ -29,7 +29,7 @@ const ItemGroup: React.FC<IGProps> = ({ children }) => {
 
 const Section2: React.FC = () => {
   return (
-    <div className="flex flex-col py-16 justify-center items-center bg-slate-300 px-2 text-center md:py-12 lg:py-10 lg:flex-row tk-acumin-pro-semi-condensed text-gray-800">
+    <div className="flex flex-col py-16 justify-center bg-slate-300 px-2 text-center md:py-12 lg:py-10 lg:flex-row tk-acumin-pro-semi-condensed text-gray-800">
       <ItemGroup>
         <SectionItem
           heading="Work with Intention"
@@ -41,11 +41,13 @@ const Section2: React.FC = () => {
           details="Young people are intelligent, responsible, capable, creative,caring, and must be central participants in shaping our collective future."
         />
       </ItemGroup>
-      <img
-        src={deliveryAngels}
-        alt="Two Delivery Angels fulfilling a delivery."
-        className="my-4"
-      />
+      <div className="basis-0 grow-[5] min-w-[450px]">
+        <img
+          src={mealPrep}
+          alt="A cook from the Teen Kitchen Project preparing a set of meals."
+          className="min-h-full object-cover object-center"
+        />
+      </div>
       <ItemGroup>
         <SectionItem
           heading="Food is Love"
