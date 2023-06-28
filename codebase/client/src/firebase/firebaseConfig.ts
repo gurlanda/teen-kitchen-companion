80d9043c4cc2b4fb0aa11757f7218bd3f4aed849
Gamliel Urlanda <g.urlanda@gmail.com>
2023-06-28 11:40:07 -0700

    client: configure firebase

---
+++ codebase/client/src/firebase/firebaseConfig.ts
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJp-_twNa9o03JzPd9VHBMhSQR4JfyQpM',
+  authDomain: 'teen-kitchen-companion.firebaseapp.com',
+  projectId: 'teen-kitchen-companion',
+  storageBucket: 'teen-kitchen-companion.appspot.com',
+  messagingSenderId: '246451943653',
+  appId: '1:246451943653:web:710639f64ebbdc2fecbc0c',
+  measurementId: 'G-CNNM1ZSTWM',
+};
+
+export default firebaseConfig;
---
+++ codebase/client/src/firebase/getFirebaseServices.ts
@@ -0,0 +1,52 @@
+import { initializeApp } from 'firebase/app';
+import * as Auth from 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+function initializeFirebase() {
+  const app = initializeApp(firebaseConfig);
+  const authRef = Auth.getAuth();
+
+  // Define the return type here for a better autocomplete experience and to keep the return type of createUserWithEmailAndPassword intentionally coupled to Auth.createUserWithEmailAndPassword
+  type t = ReturnType<typeof Auth.createUserWithEmailAndPassword>;
+  const createUserWithEmailAndPassword = (
+    email: string,
+    password: string
+  ): t => {
+    return Auth.createUserWithEmailAndPassword(authRef, email, password);
+  };
+
+  type s = ReturnType<typeof Auth.signInWithEmailAndPassword>;
+  const signInWithEmailAndPassword = (email: string, password: string): s => {
+    return Auth.signInWithEmailAndPassword(authRef, email, password);
+  };
+
+  type u = ReturnType<typeof Auth.signOut>;
+  const signOut = (): u => {
+    return Auth.signOut(authRef);
+  };
+
+  type v = ReturnType<typeof Auth.onAuthStateChanged>;
+  type vParams = Parameters<typeof Auth.onAuthStateChanged>;
+  const onAuthStateChanged = (
+    nextOrObserver: vParams[1],
+    error?: vParams[2],
+    completed?: vParams[3]
+  ): v => {
+    return Auth.onAuthStateChanged(authRef, nextOrObserver, error, completed);
+  };
+
+  return {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+  };
+}
+
+export type FirebaseServices = ReturnType<typeof initializeFirebase>;
+function getFirebaseServices(): FirebaseServices {
+  const services = initializeFirebase();
+  return services;
+}
+
+export default getFirebaseServices;
