908620f7c2d7ed2e4aa12f633eaa8ca4f7d4477e
Gamliel Urlanda <g.urlanda@gmail.com>
2023-08-13 15:01:09 -0700

    Make EllipsisMenu not activate the hover state or active state of its attached FileItem

--- codebase/client/src/components/pages/MenuEdit/components/FileItem.tsx
+++ codebase/client/src/components/pages/MenuEdit/components/FileItem.tsx
@@ -3,6 +3,8 @@ import MenuContext from '../context/MenuContext';
 import Button from './utilities/Button';
 import File from '../model/File';
 import DraggableColumnItem from './utilities/DraggableColumnItem';
+import ColumnItem from './utilities/ColumnItem';
+import { Draggable } from 'react-beautiful-dnd';
 
 const FileItem = ({
   file,
@@ -17,59 +19,64 @@ const FileItem = ({
     useState<boolean>(false);
 
   return (
-    <DraggableColumnItem
-      className="flex relative gap-2"
-      draggableId={file.id}
-      index={index}
-      onClick={() => setPreviewedFile(file.url)}
-    >
-      <input
-        type="file"
-        ref={inputRef}
-        accept="application/pdf"
-        onChange={(e) => {
-          const files = e.target.files;
-          if (!files || files.length === 0) {
-            return;
-          }
+    <Draggable draggableId={file.id} index={index}>
+      {(provided) => (
+        <div
+          className="relative"
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          <ColumnItem
+            className={` flex relative gap-2 bg-stone-300 hover:bg-stone-400 active:bg-stone-500 select-none`}
+            onClick={() => setPreviewedFile(file.url)}
+          >
+            <input
+              type="file"
+              ref={inputRef}
+              accept="application/pdf"
+              onChange={(e) => {
+                const files = e.target.files;
+                if (!files || files.length === 0) {
+                  return;
+                }
 
-          const chosenFile = files[0];
-          const fileUrl = URL.createObjectURL(chosenFile);
+                const chosenFile = files[0];
+                const fileUrl = URL.createObjectURL(chosenFile);
 
-          changeFile(index, fileUrl);
-          setPreviewedFile(fileUrl);
-        }}
-        className="h=[0.1px] w-[0.1] opacity-0 absolute -z-50"
-      />
-      <span className="basis-0 grow break-all">{file.url}</span>
-      {/* <Button onClick={onClickDelete} className="basis-0">
-        Delete file
-      </Button>
-      <Button onClick={onClickReplace} className="basis-0">
-        Replace file
-      </Button> */}
-      <button
-        className="flex items-center"
-        onClick={(e) => {
-          e.stopPropagation();
-          setIsEllipsisMenuVisible(!isEllipsisMenuVisible);
-        }}
-      >
-        <i className="fa-solid fa-ellipsis-v text-2xl" />
-      </button>
+                changeFile(index, fileUrl);
+                setPreviewedFile(fileUrl);
+              }}
+              className="h=[0.1px] w-[0.1] opacity-0 absolute -z-50"
+            />
+            <span className="basis-0 grow break-all">{file.url}</span>
+            <button
+              className="flex items-center"
+              onClick={(e) => {
+                e.stopPropagation();
+                setIsEllipsisMenuVisible(!isEllipsisMenuVisible);
+              }}
+            >
+              <i className="fa-solid fa-ellipsis-v text-2xl" />
+            </button>
+          </ColumnItem>
 
-      {/* Ellipsis menu */}
-      <div
-        className={`absolute left-[calc(100%_+_8px)] top-1/2 -translate-y-1/2 bg-white border rounded-lg overflow-hidden ${
-          isEllipsisMenuVisible ? '' : 'hidden'
-        }`}
-      >
-        <EllipsisMenuItem onClick={onClickReplace}>
-          Replace file
-        </EllipsisMenuItem>
-        <EllipsisMenuItem onClick={onClickDelete}>Delete file</EllipsisMenuItem>
-      </div>
-    </DraggableColumnItem>
+          {/* Ellipsis menu */}
+          <div
+            className={`absolute left-[calc(100%_+_8px)] top-1/2 -translate-y-1/2 bg-white border rounded-lg overflow-hidden ${
+              isEllipsisMenuVisible ? '' : 'hidden'
+            }`}
+          >
+            <EllipsisMenuItem onClick={onClickReplace}>
+              Replace file
+            </EllipsisMenuItem>
+            <EllipsisMenuItem onClick={onClickDelete}>
+              Delete file
+            </EllipsisMenuItem>
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 
   function onClickDelete(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
