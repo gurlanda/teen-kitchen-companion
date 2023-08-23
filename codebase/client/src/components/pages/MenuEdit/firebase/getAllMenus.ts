fd817a3219dd2cdb694f7d0fdde8709d0cf03f6c
Gamliel Urlanda <g.urlanda@gmail.com>
2023-08-23 00:16:50 -0700

    MenuEdit: Fix bug where menus aren't sorted by date in descending order

--- codebase/client/src/components/pages/MenuEdit/firebase/getAllMenus.ts
+++ codebase/client/src/components/pages/MenuEdit/firebase/getAllMenus.ts
@@ -1,11 +1,14 @@
-import { query } from 'firebase/firestore';
+import { orderBy, query } from 'firebase/firestore';
 import Menu from '../model/Menu';
 import getMenus from './getMenus';
 import menusCollectionRef from './menusCollectionRef';
 
 async function getAllMenus(): Promise<Menu[]> {
   try {
-    const allMenusQuery = query(menusCollectionRef);
+    const allMenusQuery = query(
+      menusCollectionRef,
+      orderBy('startDate', 'desc')
+    );
     const menus = await getMenus(allMenusQuery);
     return menus;
   } catch (error) {
