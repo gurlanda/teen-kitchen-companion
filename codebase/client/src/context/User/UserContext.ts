34b1d0955ff9660cc77b4cf77bb4c7db61e7ab4f
Gamliel Urlanda <g.urlanda@gmail.com>
2023-08-30 15:10:42 -0700

    Remove unused User context

--- codebase/client/src/context/User/UserContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-import User from 'src/model/User/User';
-
-interface UserContext {
-  user: User | undefined;
-}
-
-const userContext = createContext<UserContext | undefined>(undefined);
-export default userContext;
